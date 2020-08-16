070e0b376564490de4be136c7f7fb4be1fe7a095
alok.saboji@gmail.com”git config --global user.email alok.saboji@gmail.comgit initgit config --global user.name alok.saboji@gmail.com” <alok.saboji@gmail.com>
2020-08-17 00:28:52 +0530

    adding profile pic

---
+++ src/common/Util.js
@@ -0,0 +1,5 @@
+export const constants = {
+    mediaUrl:"https://graph.instagram.com/me/media?fields=id,caption",
+    postUrl:"https://graph.instagram.com",
+    access_token:"IGQVJWVzJacHFtMzFTZAUxPZAkdCcWEzQy1fSUZAUWFhPdElrUUdzckx4ZAEVZAM1lLOWVLVmZAYQ2hnX3p0d3p0dXQ0Y08tNnVwb1g0SnZAXVjkyVEVoUEo3Um1wZAmN0NGd2c2RPY0NsMkFaM1BxWEFHajg3VmhwOTQyOVM2SzVn"
+}
--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,11 +1,22 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import './Header.css';
+import Avatar from '@material-ui/core/Avatar';
+import UserAvatar from "../../assets/alok.jpg";
+
 class Header extends Component{
   render(){
     return(
         <div className="app-header">
-        <span className="header-logo"> Image Viewer </span>
-      </div>
+          <div className="header-logo"> Image Viewer</div>
+          <div className="header-right">
+            <div className="search">
+              <Avatar alt="Profile" src={UserAvatar} />
+            </div>
+            <div className="profile-button">
+
+            </div>
+          </div>
+        </div>
     )
   }
 }
