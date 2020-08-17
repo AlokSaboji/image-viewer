74bd7cbd7ea449c5dc94dd08c79237dde6b52e8a
alok.saboji@gmail.com”git config --global user.email alok.saboji@gmail.comgit initgit config --global user.name alok.saboji@gmail.com” <alok.saboji@gmail.com>
2020-08-18 01:14:52 +0530

    Adding Search UI

--- src/common/App.js
+++ src/common/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {  Switch, Route } from 'react-router-dom';
 import Login from '../screens/login/Login';
 import Home from '../screens/home/Home';
 
--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,18 +2,66 @@ import React, {Component} from 'react';
 import './Header.css';
 import Avatar from '@material-ui/core/Avatar';
 import UserAvatar from "../../assets/alok.jpg";
+import IconButton from '@material-ui/core/IconButton';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from "@material-ui/core/Menu";
+import Input from "@material-ui/core/Input/Input";
+import SearchIcon from '@material-ui/icons/Search';
+import InputAdornment from "@material-ui/core/InputAdornment";
+
 
 class Header extends Component{
-  render(){
+    searchChangeHandler = () => {
+
+    };
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            search:"",
+        };
+    }
+    handleClick = (event) => {
+        this.setState({anchorEl:event.currentTarget});
+    };
+
+     handleClose = () => {
+         this.setState({anchorEl:null});
+    };
+
+
+
+
+    render(){
     return(
         <div className="app-header">
           <div className="header-logo"> Image Viewer</div>
           <div className="header-right">
             <div className="search">
-              <Avatar alt="Profile" src={UserAvatar} />
+                <Input  className="search-input" type="text" placeholder={'Search...'} username={this.state.search} onChange={this.searchChangeHandler}
+                    startAdornment={
+                    <InputAdornment position="start">
+                      <SearchIcon/>
+                    </InputAdornment>
+                }>
+                </Input>
             </div>
             <div className="profile-button">
-
+                <IconButton variant="contained"
+                            aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick}>
+                    <Avatar alt="Profile" src={UserAvatar} />
+                </IconButton>
+                <Menu className="menu-container"
+                    id="simple-menu"
+                    anchorEl={this.state.anchorEl}
+                    keepMounted
+                    open={Boolean(this.state.anchorEl)}
+                    onClose={this.handleClose}
+                    >
+                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                </Menu>
             </div>
           </div>
         </div>
